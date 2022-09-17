9cf502b89219b518061a7da9432e7c612601d96f
Jasper Mai <jaspermai@v1044-wn-rt-a-132-212.campus-dynamic.uwaterloo.ca>
2022-09-17 17:36:40 -0400

    styling for inputField

--- instad/src/App.js
+++ instad/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 
 import InputField from './components/inputField.js';
-import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
-      <InputField text="Product Name" preview="ex. HydrateIt"></InputField>
+        <InputField text="Product Name" preview="ex. HydrateIt"></InputField>
+        <InputField text="Product Type" preview="ex. water bottle"></InputField>
+        <InputField text="Genre" preview="ex. Enthusiastic"></InputField>
+        <InputField text="Keywords" preview="ex. flavorful, accessible, affordable"></InputField>
     </div>
   );
 }
