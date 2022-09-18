13668553167946e64206bbfb7eaa8b21232a52ce
Jasper Mai <jaspermai@v1044-wn-rt-a-132-212.campus-dynamic.uwaterloo.ca>
2022-09-17 20:38:55 -0400

    styling touch-ups

--- instad/src/App.js
+++ instad/src/App.js
@@ -9,18 +9,18 @@ function App() {
   return (
     <div className="App">
         <div className="flex-left">
-          <div className="flex-test">
-          </div>
         </div>
         <div className="flex-right">
           <MainText></MainText>
           <div className="flex-fields">
             <InputField text="Product Name" preview="ex. HydrateIt"></InputField>
             <InputField text="Product Type" preview="ex. water bottle"></InputField>
-            <InputField text="Genre" preview="ex. Enthusiastic"></InputField>
-            <InputField text="Keywords" preview="ex. flavorful, accessible, affordable"></InputField>
+            <InputField text="Genre" preview="ex. enthusiastic"></InputField>
+            <InputField text="Keywords" preview="ex. exciting, flavorful, affordable"></InputField>
           </div>
-          <Button className="flex-button" text="CREATE" handleClick={() => {window.alert('create')}}></Button>
+          <div className="flex-button">
+            <Button className="flex-button" text="CREATE" handleClick={() => {window.alert('create')}}></Button>
+          </div>
 
         </div>
         
