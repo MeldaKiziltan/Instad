7f38ae3a2bfefbe253017b1d6e70d3ea0cfb6409
Shaheer Rana <s32rana@uwaterloo.ca>
2022-09-18 00:28:41 -0400

    Working ZMQ pipeline for python-js communication

--- StabilityAI/imageGen.py
+++ StabilityAI/imageGen.py
@@ -1,25 +1,37 @@
-
+import zmq
 import io
 import os
 import warnings
+import json
 
 from PIL import Image
 from stability_sdk import client
 import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
 
+# ZeroMQ Context
+context = zmq.Context()
+
+# Define the socket using the "Context"
+sock = context.socket(zmq.PULL)
+sock.connect("tcp://127.0.0.1:5690")
+
+p_genre = sock.recv()
+#p_type = socket.recv()
+#p_keys = socket.recv()
+print ("%s " % (p_genre))
 
 stability_api = client.StabilityInference(
     key='sk-RrxHMIj8vuBDXJhjMySKzGAolQlb9DMFod2TBUQqEr2n6sDM', 
     verbose=True,
 )
-
+#genre type, keywords
 # the object returned is a python generator
 answers = stability_api.generate(
-    prompt="a storefront that has the word 'intsAd' written on it",
-    #seed=34567, # if provided, specifying a random seed makes results deterministic
+    #prompt= "%s %s, %s" % (p_genre, p_type, p_keys),
+    prompt = str(p_genre).encode("utf-8").decode("utf-8"),
+    seed=34567, # if provided, specifying a random seed makes results deterministic
     steps=50, # defaults to 50 if not specified
 )
-
 # iterating over the generator produces the api response
 for resp in answers:
     for artifact in resp.artifacts:
@@ -31,3 +43,8 @@ for resp in answers:
             img = Image.open(io.BytesIO(artifact.binary))
             img.show()
             img = img.save("/Users/shabana/Coding/Instad/StabilityAI/ad.jpg");
+
+sock1 = context.socket(zmq.PUSH)
+sock1.connect("tcp://127.0.0.1:5600")
+sock1.send_string("ad.jpg")
+print("done")
